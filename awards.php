0d67311d466fdffc6d56e80677096493a850805c
ICMSA <marvina@icmsaust.com.au>
2020-09-15 10:22:22 +1000

    Populated awards.php and visa-information.php

--- awards.php
+++ awards.php
@@ -3,6 +3,48 @@
     include('include/header.php');
  ?>
 
+ <p>The AusACPDM is pleased to offer a series of awards to honour our outstanding members and contributors to society. The Academy recognises scientific excellence and professional and community contributions with the following awards:</p>
+
+<h2>Best Paper and Best Poster</h2>
+<p><b>Who</b>: The recipients of these awards will be the first author of the paper and the poster that receive the highest score for their abstract and paper or poster presentation.</p>
+<p><b>How</b>: Abstracts will be scored by the scientific committee of the AusACPDM. The authors of the highest scoring abstracts will then be scored on their presentation or poster by members of the Scientific and Awards Committees. See scoring matrix.</p>
+<p><b>When</b>: The winners of best paper and best poster are announced at the Conference closing ceremony.</p>
+
+<h2>Cerebral Palsy Alliance Research Foundation: Best Prevention or Cure Paper</h2>
+<div class="row">
+    <div class="col-6 col-lg-3"><h3>Sponsored by:</h3></div>
+    <div class="col col-lg"><img src="./img/sponsors/cerebral-palsy-alliance.jpg" alt="Cerebral Palsy Alliance" width="120"></div>
+</div>
+
+
+<p><b>Who</b>: The recipient of this award will be the first author of the prevention/cure paper that received the highest score for their abstract and presentation.</p>
+<p><b>How</b>: The scientific committee will score each abstract. The highest scoring abstracts will then be scored on their presentation by members of the Scientific and Awards Committees. See scoring matrix.</p>
+<p><b>When</b>: Winners are announced at the Conference closing ceremony.</p>
+
+<h2>Promising Career Award</h2>
+<p><b>Who</b>: The recipient of this award will be an Academy member from Australasia presenting at the Conference who is currently a student or within four years of completion of training (clinical or research training).</p>
+<p><b>How</b>: Presenters can self-select that they are eligible for the award during registration. The scientific committee will score each abstract. The highest scoring eligible abstracts will then be scored on their presentation by members of the Awards Committee. See scoring matrix.</p>
+<p><b>When</b>: The winner will be announced at the Conference closing ceremony.</p>
+
+<h2>Transformative Practice Award</h2>
+ <p><b>Who</b>: The recipient of this award will be someone who has made an outstanding contribution to Cerebral Palsy and Developmental Medicine. The intent of the Transformative Practice Award is to acknowledge a clinician of any discipline who actively facilitates the translation of research into practice and encourages colleagues to be evidence-based clinicians. Nominators, seconders and nominees must be members of AusACPDM.</p>
+ <p><b>How</b>: An open letter must be submitted to the Awards and Advocacy Committee, outlining the nominee’s contribution to the uptake of evidence based practice. Please also include nominator and nominee contact information, length of time and in what role nominators have known the nominee. The nominating letter should be limited to 400 words.</p>
+
+<h2>Person with Lived Experience – Outstanding Contribution</h2>
+ <p><b>Who</b>: The recipient of this award will be a person with cerebral palsy or a developmental disability who has made a distinguished contribution to knowledge, health or wellbeing of others with cerebral palsy or developmental disability. Nominators and seconders must be members of AusACPDM.</p>
+ <p><b>How</b>: An open letter from a nominator and a seconder must be submitted to the Awards and Advocacy Committee outlining the nominee’s important contribution through a) scientific and or practical contributions or b) advocacy or leadership in promoting health and/or wellbeing of people with</p>a disability. Please include nominator and nominee contact information, length of time and in what role nominators have known the nominee. The nominating letter should be limited to 400 words.
+
+<h2>Local Hero Award</h2>
+ <p><b>Who</b>: The recipient of this award will be acknowledged for their outstanding contribution to the community in any facet of life. Nominators, seconders and nominees do not need to be AusACPDM members. The nominated person will have cerebral palsy or a developmental disability, reside in the area of the Conference and can be any age from 12 years into adulthood.</p>
+ <p><b>How</b>: Please email a letter (no more than 400 words) or a video (no more than 3 minutes) that outlines the contributions made by this person. The winner will be selected based on the Awards and Advocacy Committee scoring and will be announced at the opening ceremony.</p>
+
+<h2>Further Enquiries</h2>
+<h4>For further information on AUSACPDM Awards, please contact:</h4>
+ <p><b>Sarah McIntyre</b>
+     <a href="mailto:awards@ausacpdm.org.au">awards@ausacpdm.org.au</a></p>
+
+
+
 <?php
     include('include/footer.php');
  ?>
--- visa-information.php
+++ visa-information.php
@@ -3,6 +3,92 @@
     include('include/header.php');
  ?>
 
+
+<h2>Deadlines</h2>
+<p>We recommend that all delegates apply for their Australian visa a <b>minimum of 2 months prior</b> to their arrival in the country.</p>
+
+<h2>Visa Options for Conferences</h2>
+ <p>All visitors to Australia must have a valid visa to travel to and enter Australia (other than New Zealand passport holders who will normally be granted a Special Category visa on arrival, provided they meet health and character requirements; and permanent residents of Norfolk Island who may be granted a Permanent Resident of Norfolk Island visa on arrival).</p>
+ <p>The appropriate visa will depend, amongst other things, on your purpose for visiting Australia.</p>
+ <p>The most common Visa’s that may be recommended to you for short travel to Australia to attend a conference are;</p>
+<h3>1. ETA (601)</h3>
+<ul>
+    <li>Online Application Process</li>
+    <li>Quick</li>
+</ul>
+ <p>Eligibility for an eVisitor visa and ETA will be determined by the country of passport that the applicant holds. The ETA is a quick, online, charge free process, but incurs a AUD$20 service provider charge, and/or a further charge if applied for through a travel agency, airline agency or specialist service provider. Further information on the ETA can be found on the <a href="https://immi.homeaffairs.gov.au/visas/getting-a-visa/visa-listing/electronic-travel-authority-601" target="_blank">website</a>.</p>
+<h3>2. eVisitor (651)</h3>
+<ul>
+    <li>Online Application Process</li>
+    <li>Quick</li>
+    <li>One working day processing time</li>
+    <li>No Application charge or service fee</li>
+</ul>
+ <p>Eligibility for an eVisitor visa and ETA will be determined by the country of passport that the applicant holds. The eVisitor visa is an online visa process, with a one working day processing time for low risk applications. There is no visa application charge or service fee. Further information on the eVisitor visa, including how to apply online, can be found on the <a href="https://immi.homeaffairs.gov.au/visas/getting-a-visa/visa-listing/evisitor-651" target="_blank">website</a>.</p>
+<h3>3. Visitor Visa (subclass 600) in the Business Visitor stream</h3>
+<ul>
+    <li>Online or Paper application Process</li>
+</ul>
+<p>Passport holders who are not eligible to apply for an eVisitor or an ETA should apply for a Visitor Visa (subclass 600) in the Business Visitor Stream.</p>
+<ul>
+    <li>Online application process</li>
+    <li>Application fee of $140 AUD</li>
+</ul>
+<p>Further information on the Subclass 600 Visa can be found on the <a href="https://immi.homeaffairs.gov.au/visas/getting-a-visa/visa-listing/visitor-600/business-visitor-stream" target="_blank">website</a>.</p>
+
+<h2>Invitation and registration to attend the event.</h2>
+ <p>Although this is a separate process to lodging your visa application, when assessing whether you meet the criteria for the grant of a visa, the department may take into account whether you are registered or were invited to attend the event.</p>
+ <p>Confirmation of registration, or a copy of your letter of invitation, should accompany visa applications. For online subclass 400 and subclass 600 applications, information about documents that need to be attached will be provided as part of the application process. Your letter of invitation will be sent to you with your registration successful letter.</p>
+
+<h2>Things to consider before lodging an application</h2>
+<h3>Health and Character Requirements</h3>
+ <p>All visa applicants must be assessed against Australia’s health and character requirements. These requirements are designed to protect the safety and security of the Australian community.</p>
+<h4>Health requirements</h4>
+<p>As part of the visa application process you may be required to undertake a medical examination, chest x ray and/or other health checks.</p>
+<p>The Department of Immigration and Border Protection (the department) will advise you of the applicable health checks (if any), based on your individual circumstances.</p>
+<p>Information on which health checks may apply to you is available on the department’s website.</p>
+<p>See: <a href="http://www.border.gov.au/Trav/Visa/Heal" target="_blank">www.border.gov.au/Trav/Visa/Heal</a></p>
+<h4>Character requirements</h4>
+<p>As part of the visa application assessment process the onus is on you to satisfy the character requirements set out in Section 501 of the Migration Act 1958. Further information on the character requirement is available on the department’s website. See: www.border.gov.au/Trav/Visa/Char</p>
+<p>If you are unsure whether you satisfy the character requirements because of prior criminal convictions, you should lodge a paper-based application at the Australian visa office nearest to your current place of residence, at least four months prior to the event start date.</p>
+<p>Contact information for Australian visa offices is available on the department’s website.</p>
+<p>See: <a href="http://www.border.gov.au/about/contact/offices-locations" target="_blank">www.border.gov.au/about/contact/offices-locations</a></p>
+<h3>Travel sanctions</h3>
+<p>For information on countries subject to travel sanctions, please refer to the Department of Foreign Affairs and Trade’s website. Nationals of these countries may not be permitted to travel to and enter Australia.</p>
+<p>See: <a href="http://www.dfat.gov.au/international-relations/security/sanctions/pages/sanctions.aspx" target="_blank">www.dfat.gov.au/international-relations/security/sanctions/pages/sanctions.aspx</a></p>
+ <h3>Biometrics programme</h3>
+ <p>Biometrics may need to be collected at an Australian Visa Application Centre for visitors and business people lodging a visa application from the countries listed on the department’s website.</p>
+ <p>See: <a href="http://www.border.gov.au/Trav/Visa/Biom" target="_blank">www.border.gov.au/Trav/Visa/Biom</a></p>
+ <p>If you lodge your application online, the department will advise you in writing if you need to provide your biometrics.</p>
+
+<h2>Methods of submitting your visa application</h2>
+<h3>Online application</h3>
+ <p>ImmiAccount is a single point of entry into the department’s online visa services. ImmiAccount accepts all online applications including eVisitor visa (subclass 651), Visitor e600 visa and Temporary Work (Short Stay Activity) visa (subclass 400).</p>
+ <p>ImmiAccount does not accept applications for Electronic Travel Authority (ETA).</p>
+<p>More information about ImmiAccount is available on the department’s website. <br>
+    See: <a href="http://www.border.gov.au/immiaccount" target="_blank">www.border.gov.au/immiaccount</a></p>
+<p>To check if you are eligible to apply for an Electronic Travel Authority (ETA) (subclass 601). <br>
+    See: <a href="http://www.border.gov.au/Trav/Visa-1/601-" target="_blank">www.border.gov.au/Trav/Visa-1/601-</a></p>
+<p>To check if you are eligible to apply for an eVisitor visa (subclass 651). <br>
+    See: <a href="http://www.border.gov.au/Trav/Visa-1/651-" target="_blank">www.border.gov.au/Trav/Visa-1/651-</a></p>
+ <p>To check if you are eligible to apply for an online Visitor e600 visa. <br>
+     See: <a href="http://www.border.gov.au/Trav/Visa-1/600-" target="_blank">www.border.gov.au/Trav/Visa-1/600-</a></p>
+<p>To check if you are eligible to apply for an online Temporary Work (Short Stay Activity) visa (subclass 400). <br>
+    See: <a href="http://www.border.gov.au/Trav/Visa-1/400-" target="_blank">www.border.gov.au/Trav/Visa-1/400-</a></p>
+<p>To attach documents to your online visa application, you will need to login to your ImmiAccount. <br>
+    See: <a href="http://www.border.gov.au/Trav/Visa/Atta" target="_blank">www.border.gov.au/Trav/Visa/Atta</a></p>
+
+<h2>Obligations of visa holders</h2>
+<p>On arrival at Australia’s border you must identify yourself, present your travel documents, and complete an Incoming Passenger Card (IPC). The IPC requires visitors to declare their health status and any prior criminal convictions.</p>
+<p>If you do not meet immigration clearance requirements, you may be refused entry to Australia.</p>
+<p>More information on immigration clearance processes at Australia’s borders is available on the department’s website.</p>
+<p>See: <a href="http://www.border.gov.au/Busi/Comp">www.border.gov.au/Busi/Comp</a></p>
+<p>You must comply with your visa validity and visa conditions while in Australia. When you receive your Australian visa you are issued with a visa grant notice that explains the conditions of the visa including period of validity and entry requirements. You are advised to carry this visa grant notice when travelling for your own reference.</p>
+<p>Your visa may be cancelled if you breach any of the conditions.</p>
+<p>If your visa expires while you are still in Australia, you become an unlawful non-citizen. Unlawful non-citizens are liable to be detained and removed from Australia.</p>
+
+
+
 <?php
     include('include/footer.php');
  ?>
