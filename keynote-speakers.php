2f6376c3f313f03baabc444ae1989493de8d67ce
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-18 10:29:24 +1100

    Text edits and making modal links into buttons

--- keynote-speakers.php
+++ keynote-speakers.php
@@ -3,11 +3,15 @@
     include('include/header.php');
  ?>
 
+ <h4><em>More speakers to be announced soon</em></h4>
+
  <img src="img/keynote/king.png" class="img-fluid float-right ml-4 mb-4 mt-4" width="200">
  <h2 class="mb-0">Prof. Gillian King</h2>
  <h3 class="mt-0">Canada</h3>
 
- <p>Gillian King is a Distinguished Senior Scientist (Bloorview Research Institute), Professor in the Department of Occupational Science and Occupational Therapy (University of Toronto), and Canada Research Chair in Optimal Care for Children with Disabilities (Tier 1). She has held over 100 peer-reviewed grants, authored over 200 peer-reviewed articles, and has developed and published over 20 measures for use in pediatric rehabilitation. Her work focuses on psychosocial aspects of care, including the delivery of health services, client engagement, family-centred care, client and family resiliency, and transition programming. Her current research focuses on measures of client, parent, and therapist engagement in service delivery, the key elements and processes underlying the effectiveness of youth transition programs, mental health and the participation of children and youth with disabilities, solution-focused coaching, and the role of resiliency in rehabilitation service delivery. <br><br> <a href="#king" data-toggle="modal">Get to know Gillian</a></p>
+ <p>Gillian King is a Distinguished Senior Scientist (Bloorview Research Institute), Professor in the Department of Occupational Science and Occupational Therapy (University of Toronto), and Canada Research Chair in Optimal Care for Children with Disabilities (Tier 1). She has held over 100 peer-reviewed grants, authored over 200 peer-reviewed articles, and has developed and published over 20 measures for use in pediatric rehabilitation. Her work focuses on psychosocial aspects of care, including the delivery of health services, client engagement, family-centred care, client and family resiliency, and transition programming. Her current research focuses on measures of client, parent, and therapist engagement in service delivery, the key elements and processes underlying the effectiveness of youth transition programs, mental health and the participation of children and youth with disabilities, solution-focused coaching, and the role of resiliency in rehabilitation service delivery. </p>
+
+ <a href="#king" data-toggle="modal" class="button mt-0">Get to know Gillian</a>
 
  <hr>
 
@@ -15,7 +19,9 @@
  <h2 class="mb-0">A/Prof. Mark Peterson</h2>
  <h3 class="mt-0">USA</h3>
 
- <p>Mark D. Peterson is the Charles E. Lytle, Jr. Research Professor at the University of Michigan-Medicine (Ann Arbor, Michigan, U.S.), Department of Physical Medicine and Rehabilitation, a Fulbright Scholar, and an active member in the American Academy of Cerebral Palsy and Developmental Medicine (AACPDM), Australasian Academy of Cerebral Palsy and Developmental Medicine (AusACPDM), the International Society of Physical and Rehabilitation Medicine (ISPRM), and the American College of Sports Medicine (ACSM). Dr. Peterson’s work focuses on understanding factors that influence health and life expectancy among individuals aging with disabilities across the lifespan. This includes efforts directed at identifying precision strategies to prevent cardiometabolic dysregulation and secondary physical and psychological morbidity among children and adults with cerebral palsy, as well as a variety of frailty syndromes, and to better understand health disparities among individuals with disabilities from the context of access to preventive care and community wellness. Dr. Peterson has published more than 150 peer reviewed scientific articles in the fields of exercise physiology, rehabilitation research, physical activity epidemiology, healthcare disparities, and preventive medicine. <br><br> <a href="#peterson" data-toggle="modal">Get to know Mark</a></p>
+ <p>Mark D. Peterson is the Charles E. Lytle, Jr. Research Professor at the University of Michigan-Medicine (Ann Arbor, Michigan, U.S.), Department of Physical Medicine and Rehabilitation, a Fulbright Scholar, and an active member in the American Academy of Cerebral Palsy and Developmental Medicine (AACPDM), Australasian Academy of Cerebral Palsy and Developmental Medicine (AusACPDM), the International Society of Physical and Rehabilitation Medicine (ISPRM), and the American College of Sports Medicine (ACSM). Dr. Peterson’s work focuses on understanding factors that influence health and life expectancy among individuals aging with disabilities across the lifespan. This includes efforts directed at identifying precision strategies to prevent cardiometabolic dysregulation and secondary physical and psychological morbidity among children and adults with cerebral palsy, as well as a variety of frailty syndromes, and to better understand health disparities among individuals with disabilities from the context of access to preventive care and community wellness. Dr. Peterson has published more than 150 peer reviewed scientific articles in the fields of exercise physiology, rehabilitation research, physical activity epidemiology, healthcare disparities, and preventive medicine.</p>
+
+ <a href="#peterson" data-toggle="modal" class="button mt-0">Get to know Mark</a>
 
  <hr>
 
@@ -23,7 +29,9 @@
  <h2 class="mb-0">Prof. Tom Shakespeare</h2>
  <h3 class="mt-0">United Kingdom</h3>
 
- <p>Tom Shakespeare trained at Cambridge University, and has worked at Sunderland, Leeds, Newcastle, University of East Anglia and London School of Hygiene and Tropical Medicine, where he is professor of disability research.  He has conducted research on disability and sexuality, with disabled children, on bioethics, on restricted growth, on independent living, on rehabilitation, on inclusive education and on employment of disabled people.  He also spent five years in the WHO Disability and rehabilitation team.  His books include Disability Rights and Wrongs and Disability – the Basics.  He broadcasts regularly on BBC Radio. <br><br> <a href="#shakespeare" data-toggle="modal">Get to know Tom</a></p>
+ <p>Tom Shakespeare trained at Cambridge University, and has worked at Sunderland, Leeds, Newcastle, University of East Anglia and London School of Hygiene and Tropical Medicine, where he is professor of disability research.  He has conducted research on disability and sexuality, with disabled children, on bioethics, on restricted growth, on independent living, on rehabilitation, on inclusive education and on employment of disabled people.  He also spent five years in the WHO Disability and rehabilitation team.  His books include Disability Rights and Wrongs and Disability – the Basics.  He broadcasts regularly on BBC Radio.</p>
+
+ <a href="#shakespeare" data-toggle="modal" class="button mt-0">Get to know Tom</a>
 
  <!-- Modal Content -->
  <div class="modal fade" id="king">
@@ -42,7 +50,7 @@
               <h3>What makes Conferences such as this important?</h3>
               <p><em>The key value to me is the coming together of like-minded people with similar interests. Conferences such as this serve to develop a ‘community of practice’ that embraces researchers, people with lived experience, clinicians, students, and educators.</em></p>
 
-              <h3>What is the best piece of advice you could give a student or Early Career Researcher who is attending their first big Conference?</h3>
+              <h3>What is the best piece of advice you could give a student or early career researcher who is attending their first big conference?</h3>
               <p><em>Try to meet as many people as possible, you never know what connections you may find in terms of shared interests.</em></p>
 
               <h3>What are you most looking forward to at the Better Together 2022 Conference?</h3>
@@ -72,13 +80,13 @@
                <h3>Highlight the international relevance of your work for delegates?</h3>
                <p><em>Although my work focuses on clinical and population-representative cohorts from the United States, our work is highly relevant to the growing adult CP population, globally. Improved clinical and public health infrastructure for screening, treating, and preventing chronic disease and mental health disorders is of international relevance.</em></p>
 
-               <h3>What key messages will you have for community Members and people with lived experience of child-onset disability?</h3>
+               <h3>What key messages will you have for community members and people with lived experience of child-onset disability?</h3>
                <p><em>Adults with CP and other childhood-onset disabilities consistently report feeling deserted by their health professionals when they are discharged from paediatric services, and struggle to access appropriate continuity of care when they begin experiencing declines in health and function as early as young adulthood. I will convey to community members and people with lived experience (or caregivers) to strongly advocate for themselves, and demand comprehensive preventive medicine screening</em></p> and/or referrals to specialists to identify risk for early-onset (and preventable) metabolic, cardiovascular, musculoskeletal diseases, as well as mental health disorders.
 
-               <h3>What makes Conferences such as this important?</h3>
+               <h3>What makes conferences such as this important?</h3>
                <p><em>Conferences such as this are so valuable to me. I greatly appreciate the opportunity to learn from my long-time and new friends and colleagues, as well as network and brainstorm future collaborations, visit and experience different cultures, seek adventures, and explore new places.</em></p>
 
-               <h3>What is the best piece of advice you could give a student or Early Career Researcher who is attending their first big Conference?</h3>
+               <h3>What is the best piece of advice you could give a student or early career researcher who is attending their first big Conference?</h3>
                <p><em>Connect with other attendees and network as much as possible. Don’t be afraid to reach out to senior investigators… everyone is excited to help.</em></p>
 
                <h3>What are you most looking forward to at the Better Together 2022 Conference?</h3>
@@ -107,13 +115,13 @@
               <h3>Highlight the international relevance of your work for delegates?</h3>
               <p><em>I have researched in Africa and Asia as well as in UK, so I am aware of the differences for low, middle and high income country settings.  The need to change attitudes towards people with disabilities is common across the board.</em></p>
 
-              <h3>What key messages will you have for community Members and people with lived experience of child-onset disability?</h3>
+              <h3>What key messages will you have for community members and people with lived experience of child-onset disability?</h3>
               <p><em>Collaborating with good academics is worth doing.  It will grow your skills and insights and confidence.  Working with professionals is all about good partnerships.</em></p>
 
-              <h3>What makes Conferences such as this important?</h3>
+              <h3>What makes conferences such as this important?</h3>
               <p><em>Conferences are a great way of getting an overview of a field, of networking with your peers, and trying out your ideas.</em></p>
 
-              <h3>What is the best piece of advice you could give a student or Early Career Researcher who is attending their first big conference?</h3>
+              <h3>What is the best piece of advice you could give a student or early career researcher who is attending their first big conference?</h3>
               <p><em>Talk to strangers – networking is what it’s all about.</em></p>
 
               <h3>Do you see any particular benefits of having the conference in Melbourne/ Australia?</h3>
@@ -122,7 +130,7 @@
               <h3>Do you have any hobbies outside of your work?</h3>
               <p><em>Art and culture, good food, and creative writing.</em></p>
 
-              <h3>What was your Guilty COVID isolation pleasure?</h3>
+              <h3>What was your guilty COVID isolation pleasure?</h3>
               <p><em>Cheese.</em></p>
 
 
