2574e6a0a057dba30da9aabbfd7e264587067d61
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-02-15 15:48:42 +1100

    Added call for abstracts page

---
+++ call-for-abstracts.php
@@ -0,0 +1,135 @@
+<?php
+    $title = "Call for Abstracts";
+    include('include/header.php');
+ ?>
+
+ <p>The Scientific Committee invites you to submit abstracts now for scientific free papers, posters, instructional courses, breakfast seminars, PhD platforms and other presentation types. We encourage multi-centre, multi-country submissions from both high and low resource settings. This will be a hybrid conference and presenters can plan to attend in person or virtually. We welcome in-person, virtual and hybrid presentations. This does not need to be specified at the time of your submission.</p>
+ <p>To accommodate a hybrid conference format, all presenters are asked to consider how best to engage with both virtual and in-person attendees within their sessions to ensure an effective learning opportunity is provided. This is particularly important for breakfast seminars and instructional courses.</p>
+ <h2>Meeting policy reminders:</h2>
+ <ul>
+   <li>Abstracts that have been presented at other meetings may be submitted if they have not been published as a full research paper.</li>
+   <li>Abstracts accepted must be unbiased and conflicts of interest must be declared. Abstracts will be non‐promotional in nature and without commercial bias. Papers that are written in a manner that promotes a company, service, or product will not be considered.</li>
+   <li>All presenters of accepted abstracts are required to register for the conference (in person or virtual). Fees are not reduced or waived for presenters. An alternate author should be prepared to attend and present if an emergency causes the primary author’s absence.</li>
+ </ul>
+
+ <h2>Expectations of abstract submissions:</h2>
+ <ul>
+   <li>Each author listed on the abstract is required to complete a Financial Disclosure, which will be found in the “Presenters and Authors” section of the submission site.</li>
+   <li>Abstracts should be submitted in English in “people-first” language and avoid potentially pejorative terms, such as “wheel-chair bound”, “afflicted”, or any other terms that may be deemed insensitive to persons with disabilities.</li>
+   <li>It is expected that all research involving human subjects or animals has been approved by the appropriate ethical review committee.</li>
+ </ul>
+
+ <h4 style="color: red"><b>The deadline for submission is: 23:59 (AEST) 30th April 2021.</b></h4>
+ <p>Submissions for free papers, posters, instructional courses, breakfast seminars and PhD Platforms are welcomed over a wide range of conditions and themes including:</p>
+
+ <h3>Conditions</h3>
+ <ul>
+   <li>Acquired brain injury</li>
+   <li>Autism Spectrum Disorder</li>
+   <li>Cerebral palsy</li>
+   <li>Cerebral visual impairment</li>
+   <li>Chromosomal disorders</li>
+   <li>Neonatal Brachial Plexus palsy</li>
+   <li>Neuromuscular disorders</li>
+   <li>Spinal Bifida</li>
+   <li>Spinal cord injury and disease</li>
+   <li>Other</li>
+ </ul>
+
+ <h3>Themes</h3>
+ <ul>
+   <li>Adults and transitional care</li>
+   <li>Aetiology, epidemiology and causal mechanisms</li>
+   <li>Care in lower resource settings</li>
+   <li>Clinical practice guidelines, care pathways</li>
+   <li>Cognitive rehabilitation</li>
+   <li>Communication and assistive devices</li>
+   <li>Consumer engagement, social policy and service delivery</li>
+   <li>Disability management during a pandemic / disaster</li>
+   <li>Early detection, neonatal care and early interventions</li>
+   <li>Family/caregiver health and wellbeing</li>
+   <li>Genomics</li>
+   <li>Indigenous and First Nations people and disability</li>
+   <li>Intensive motor interventions</li>
+   <li>Knowledge translation and implementation</li>
+   <li>Living and planning for pandemics and disasters</li>
+   <li>Mental health and wellbeing</li>
+   <li>Movement disorders and hypertonia/hypotonia management</li>
+   <li>Neuroscience and neuroimaging</li>
+   <li>New technologies, innovations and robotics</li>
+   <li>Nutrition, diet, feeding and oropharyngeal dysphagia</li>
+   <li>Orthopaedics, muscle and bone health</li>
+   <li>Pain</li>
+   <li>Participation</li>
+   <li>Physical activity, fitness and sports/leisure</li>
+   <li>Physical rehabilitation/habilitation</li>
+   <li>Preclinical studies, neuroprotection, prevention & neural repair</li>
+   <li>Sleep</li>
+   <li>Telehealth</li>
+   <li>Vision</li>
+   <li>Other</li>
+ </ul>
+
+ <a href="https://icmsaust.eventsair.com/ausacpdmiaacd-2022---childhood-disabilities/symposia" target="_blank" class="button d-block text-center">Submit your abstract here</a>
+
+ <p>Members of the Better Together 2022 Scientific Program Committee will review the abstracts, blinded to the names of the authors and institutions affiliated with the submission. Presentations will be selected on the basis of their quality, relevance and impact determined by review of the research question or hypothesis, study design, methodology, conclusions, impact/relevance and importance as described in the abstract. Student status of the author will not affect the abstract submission type or the grading of the abstract.</p>
+
+ <h2>Presentation Types</h2>
+ <p>The Scientific Program Committee determines the presentation format of scientific presentations after review and selection. In order to make fair comparisons of the quality of submitted abstracts across a wide spectrum of disciplines, it is imperative that the format of a structured abstract be followed. A strict 400 word count, will be imposed on the length of the body of the abstract.</p>
+ <p>The presentation types available are, also linked to their individual templates with specific guidelines:</p>
+ <ul>
+   <li><a href="./downloads/Free-Papers.docx" target="_blank">Free papers</a></li>
+   <li><a href="./downloads/Scientific-Demonstration-Posters.docx" target="_blank">Scientific/demonstration posters</a></li>
+   <li><a href="./downloads/Case-Study-Clinical-Obs-Posters.docx" target="_blank">Case study/clinical observation posters</a></li>
+   <li><a href="./downloads/Breakfast-Seminars.docx" target="_blank">Breakfast seminars</a></li>
+   <li><a href="./downloads/Instructional-Course-Workshop.docx" target="_blank">Instructional courses/workshops</a></li>
+   <li><a href="./downloads/PhD-Platform-Guidelines-&-Application-Form.docx" target="_blank">PhD platforms</a></li>
+ </ul>
+
+ <h2>Abstract Guidelines</h2>
+
+ <p>You will need to complete each section of the abstract body as follows. Note full templates are provided for your convenience.</p>
+
+ <p><b>Background/objectives:</b> Provide a brief statement about the problem or question/s of interest followed by the specific objective/s of the study. Include a list of hypotheses, when applicable.</p>
+
+ <p><b>Study design:</b> Listed below are some useful terms.</p>
+ <ul>
+   <li>Systematic review or meta-analysis</li>
+   <li>Care pathways or clinical practice guidelines</li>
+   <li>Studies of therapy/ interventions/ clinical trials:</li>
+   <li>Randomised controlled trial: (double) blinded</li>
+   <li>Placebo controlled</li>
+   <li>Crossover; before-after trial</li>
+   <li>Cohort study (retrospective or prospective)</li>
+   <li>Case control study</li>
+   <li>Studies of prognosis</li>
+   <li>Cohort study (retrospective or prospective)</li>
+   <li>Case series</li>
+   <li>Studies of diagnosis</li>
+   <li>Validating cohort study or exploratory cohort study</li>
+   <li>Non-consecutive study or case-control study</li>
+   <li>Ecological study</li>
+   <li>Health economic evaluations:</li>
+   <li>Cost-effectiveness</li>
+   <li>Cost utility or benefit analysis</li>
+   <li>Instrument development & validation study</li>
+   <li>Descriptive studies (survey, cross sectional)</li>
+   <li>Qualitative (e.g. grounded theory, phenomenology, etc.)</li>
+   <li>Describe methodology</li>
+ </ul>
+
+ <p><b>Study participants & setting:</b> Describe who is being studied and in what setting, in order to determine the generalisability of the results. The selection criteria, number who participated out of the number eligible and pertinent socio-demographic information may be described. Some terms used to describe participant selection include: random sample, consecutive series, convenience sample, volunteer sample, purposeful sample. The settings may include primary care setting, tertiary or referral centre, institutional setting, general community, etc.</p>
+ <p><b>Materials/methods:</b> Describe the methods used to conduct the study. Where applicable, identify the primary (and important secondary) outcome measures to be reported and what statistical methods (where applicable) or qualitative methods were used to conduct the analyses.</p>
+ <p><b>Results:</b> Scientific abstracts must contain analysed data from the completed study. Quantitative results are reported including standard deviations or 95% confidence intervals and the level of statistical significance when applicable. Results of qualitative studies must include appropriate description and interpretation.</p>
+ <p><b>Conclusions/significance:</b> Only those conclusions that can be supported by the data presented in the study should be reported. The significance or implications of the findings may be described.</p>
+
+<p>The Better Together 2022 Committee has also put together some useful ‘tips and tricks’ to assist you with planning your abstract submission, please visit the <a href="https://bettertogether2022.org/support-abstract.php" target="_blank">webpage here</a> to find out more.</p>
+<p>If you have questions regarding submitting an abstract, please contact the Better Together Conference Secretariat – <a href="mailto:program@bettertogether2022.org">program@bettertogether2022.org</a></p>
+
+
+
+
+
+<?php
+    include('include/footer.php');
+ ?>
