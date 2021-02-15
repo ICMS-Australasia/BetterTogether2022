708395d780992af87bfb8fb68afbb2f438b1c2de
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-02-15 15:24:53 +1100

    Content added

--- conference-venue.php
+++ conference-venue.php
@@ -12,7 +12,9 @@
 <p>The Melbourne Convention and Exhibition Centre is superbly located on the banks of Melbourne's Yarra River, close to the heart of the CBD and just 30 minutes from Melbourne's International Airport. The new Convention Centre has received worldwide acclaim for achieving a 6 Star Green Star environmental rating by the Green Building Council of Australia, the first in the world for a convention centre.</p>
 <p>The modern complex is a landmark in the area, perched on the south bank of the Yarra River, and surrounded on all sides by trams, trains, buses, taxi ranks and ample car parking. A short stroll along the iconic Southbank Promenade places you in the heart of Melbourne city – Australia's culture, cuisine and events capital – where visitors can enjoy a perfect café latte or award-winning local wine at any time of day or night.</p>
 <p>The venue is an easy walking distance from a range of accommodation options, which will be available to delegates to book at the time of registering for the conference.</p>
+<p>The venue has many accessible features, download the guide by using the following link for more information. When you arrive at the venue, please see the Customer Service team if you have any queries or require assistance.</p>
 
+<a href="https://mcec.com.au/-/media/mcec/documents/visit-information/mel0070_297x210_accessibility-features_document_online_fa.pdf?la=en&hash=207DE18AFCE70C095E03CCC717FEF01E93DC44D2" target="_blank" class="button d-block text-center">Download the MCEC accessibility guide</a>
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12606.202836305327!2d144.9531856!3d-37.8239915!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d5173a284b5%3A0x958eeaf04deea384!2sMelbourne%20Convention%20and%20Exhibition%20Centre%20(MCEC)!5e0!3m2!1sen!2sau!4v1600061645750!5m2!1sen!2sau" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 <?php
