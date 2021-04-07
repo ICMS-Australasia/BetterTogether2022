16c5fac30e796524d9e4a0a11ffd39853b470400
Marvinja <marvinjohnagbuya@gmail.com>
2021-04-07 14:55:51 +1000

    Updated pre-conference sessions

--- pre-conference-sessions.php
+++ pre-conference-sessions.php
@@ -14,9 +14,10 @@
 
  <h5 class="teal mt-4"><em>All full day pre-conference sessions will run from 9:00am – 5:30pm (AEDT) and are available for both in-person and virtual registration.</em></h5>
 
- <p><b>Improving the health and wellbeing of adults with cerebral palsy</b></p>
+ <p><b>Improving the health and wellbeing of adults with cerebral palsy </b></p>
  <p><em>Mark Peterson, Edward Hurvitz, Jan Willem Gorter, Elisabet Rodby-Bousquet, Sarah Reedman, Dinah Reddihough, Sevastine Katsakis, David Coghill, Prue Morgan, Rob Carter</em></p>
- <p>This workshop will include international experts discussing how we can meet the medical, rehabilitation and participatory needs of adults with cerebral palsy. This will include the role of physical and mental health and prevalence and risk factors for cardiovascular and musculoskeletal disease. We will integrate knowledge translation: embedding the priorities of adults living with cerebral palsy with a focus on building supportive health and disability service environments and incorporating health economics into research to drive policy and practice.</p>
+ <p>This workshop will include international experts discussing how we can meet the medical, rehabilitation and participatory needs of adults with cerebral palsy. This will include the role of physical and mental health and prevalence and risk factors for cardiovascular and musculoskeletal disease. We will integrate knowledge translation: embedding the priorities of adults living with cerebral palsy with a focus on building supportive health and disability service environments and
+incorporating health economics into research to drive policy and practice.</p>
 
 <hr>
 
@@ -27,8 +28,8 @@
 <hr>
 
  <p><b>Early Intervention for infants with cerebral palsy or born preterm</b></p>
- <p><em>Cath Morgan, Iona Novak, Ann Christian Eliasson, Stacey Dusing, Ros Boyd, Alicia Spittle, Laura Prosser and Reggie Harborne</em></p>
- <p>This workshop will outline the current state of the science in 2022 on early intervention for cerebral palsy. This pre-conference symposium will include a multidisciplinary global panel of experts and present the key ingredients and results of novel early intervention clinical trials. The day will be include discussion on statistical implementation strategies, statistical challenges and future research opportunities.</p>
+ <p><em>Cathy Morgan, Iona Novak, Ann Christian Eliasson, Stacey Dusing, Ros Boyd, Alicia Spittle, Laura Prosser, Regina Harbourne </em></p>
+ <p>This workshop will outline the current state of the science in 2022 on early intervention for cerebral palsy. This pre-conference symposium will include a multidisciplinary global panel of experts and present the key ingredients and results of novel early intervention clinical trials. The day will include discussion on implementation strategies, statistical challenges and future research opportunities.</p>
 
 <hr>
 
@@ -40,14 +41,14 @@
 
  <p><b>International CP Genomics Consortium</b></p>
  <p><em>Michael Fahey, Michael Kruer, Joseph Getz, Sarah McIntyre, Yana Wilson</em></p>
- This pre-conference workshop will discuss the latest evidence, current controversies and potential solutions related to genetics and cerebral palsy. The workshop includes international experts in the field from a variety of clinical specialties to provide a global view on current clinical and research experiences.
+ <p>This pre-conference workshop will discuss the latest evidence, current controversies and potential solutions related to genetics and cerebral palsy. The workshop includes international experts in the field from a variety of clinical specialties to provide a global view on current clinical and research experiences.</p>
 
 <hr>
 
  <h5 class="teal mt-4"><em>This pre-conference session will run from 2:00pm – 5:30pm (AEDT) and is available for both in-person and virtual registration.</em></h5>
  <p><b>International CP Registers</b></p>
  <p><em>Hayley Smithers-Sheedy, Sarah McIntyre, Kate Himmelmann, Guro L Andersen, Gulam Khandaker and Michael Shevell.</em></p>
- The 5th World CP Register and Surveillance Congress will be held in conjunction with the IAACD conference. This is a great opportunity to meet those who are defining the Epidemiology of CP and learn together! This interactive workshop is suitable for anyone interested in CP registers and surveillance including establishment, methods, operations, utility, training and recent findings.
+ <p>The 5th World CP Register and Surveillance Congress will be held in conjunction with the IAACD conference. This is a great opportunity to meet those who are defining the Epidemiology of CP and learn together! This interactive workshop is suitable for anyone interested in CP registers and surveillance including establishment, methods, operations, utility, training and recent findings.</p>
 
 <table class="program mt-5" width="100%" cellpadding="10">
     <tr class="heading" align="center">
@@ -74,7 +75,7 @@
  <h5 class="teal mt-4"><em>This pre-conference session will run from 8:30am – 2:30pm (AEDT) and is available for both in-person and hybrid registration.</em></h5>
  <p><b>Early Detection of cerebral palsy - experiences and challenges</b></p>
  <p><em>Cath Morgan, Iona Novak, Atul Malhotra, Esther Tantsis, Dr Samanmali Sumanasena, Sian Williams</em></p>
- <p>International Guidelines on early detection of cerebral palsy have been available for the past 5 years and have seen changes in practice globally. In this workshop, global experiences of implementation will be discussed. Examples of clinical conundrums and implementation challenges faced by clinicians in the early diagnosis of CP will be presented.</p>
+ <p>International Guidelines on early detection of cerebral palsy have been available for the past 5 years and have seen changes in practice globally. In this workshop, global experiences of translation will be discussed. Examples of clinical conundrums and implementation challenges faced by clinicians in the early diagnosis of CP will be presented.</p>
 
 <hr>
 
@@ -102,7 +103,7 @@
  <h5 class="teal mt-4"><em>This pre-conference session will run from 8:30am – 10:00am (AEDT) and is available for virtual registration only.</em></h5>
  <p><b>Implementation, challenges and impact of four parent-delivered training programmes for children with developmental disabilities in lower and middle income countries (LMIC)</b></p>
  <p><em>Hans Forssberg, Gillian Saloojee, Rachel Lassman, Katherine Benfer, Pamela Dixon</em></p>
- This symposium focuses on the implementation of four parent-led training programmes for children with developmental disabilities living in resource-constrained settings. Presenters will describe the training, implementation and outcomes of each programme. This will be followed by a discussion how challenges common to all programmes were addressed.
+ <p>This symposium focuses on the implementation of four parent-led training programmes for children with developmental disabilities living in resource-constrained settings. Presenters will describe the training, implementation and outcomes of each programme. This will be followed by a discussion how challenges common to all programmes were addressed.</p>
 
 <hr>
 
@@ -119,7 +120,7 @@
  <h5 class="teal mt-4"><em>This pre-conference session will run from 10:30am – 12:00pm (AEDT) and is available for virtual registration only.</em></h5>
  <p><b>Better Research Together: Conversations with Researchers and Young People</b></p>
  <p><em>Linda Nguyen, Bente van Oort, Eline van der Meulen, Hanae Davis, Samantha Bellefeuille, Jordan Matthews, Christopher Morris, Jan Willem Gorter, Anita Franklin, and Marjolijn Ketelaar</em></p>
- <p>This symposium, co-presented by young people and researchers, aims to: Share the experiences, opportunities, and challenges of research partnerships from Canada, the Netherlands, and the United Kingdom; and cultivate awareness of different models to guide engagement with young people in health research.</p>
+ <p>This symposium, co-presented by young people and researchers, aims to: Share the experiences, opportunities, and challenges of research partnerships from Canada, the Netherlands, and the United Kingdom; and cultivate awareness of different models to guide engagement with young people in health research. </p>
 
 <hr>
 
