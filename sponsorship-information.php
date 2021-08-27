68ba8bb64536ec00f536143d5c9ed701efa4fbd7
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-08-27 16:03:25 +1000

    Updated content

--- sponsorship-information.php
+++ sponsorship-information.php
@@ -3,8 +3,9 @@
     include('include/header.php');
  ?>
 
-<p>The AusACPDM/IAACD 2022 will provide the perfect forum for companies actively involved in Cerebral Palsy and Developmental Medicine to promote their products and services.   The Conference will provide multidisciplinary scientific education for health professionals as well as promote excellence in research and best practice clinical care for children and adults with cerebral palsy and developmental conditions.</p>
-<p>There will be many attractive sponsorship and exhibiting opportunities available to increase your company’s awareness to this audience, ranging from small to high level packages.  Packages can also be individually tailored to suit your requirements.</p>
+<p>The AusACPDM/IAACD 2022 virtual Conference will provide the perfect forum for companies actively involved in Cerebral Palsy and Developmental Medicine to promote their products and services to this vast audience.</p>
+<p>The Conference will provide multidisciplinary scientific education for health professionals as well as promote excellence in research and best practice clinical care for children and adults with cerebral palsy and developmental conditions.</p>
+<p>We have created many attractive sponsorship opportunities for the virtual platform that also include promotion before, during and after the Conference dates.  Packages can also be individually tailored to suit your requirements.</p>
 <h4>Please contact the Sponsorship and Exhibition Director for more information;</h4>
 <p><b>Andrea O'Sullivan</b> <br>
     <a href="mailto:sponsorship@bettertogether2022.org">sponsorship@bettertogether2022.org</a></p>
@@ -12,16 +13,31 @@
 <h2>Why Sponsor or Exhibit?</h2>
 
 <ul>
-    <li>Align your organisation with this important field of health and welfare provides you with the opportunity to consolidate relationships to a highly targeted audience involved in childhood disabilities.</li>
-    <li>Participate in the world’s premier Conference for childhood disabilities with over 1,500 healthcare professionals expected all under one roof</li>
-    <li>Connect with key decision makers in multidisciplinary scientific education for health professionals and researchers dedicated to the wellbeing of people with childhood-onset disabilities</li>
-    <li>Ensure your organisation stays abreast of current developments and research and use this information to stay ahead of the market</li>
-    <li>Develop business connections and strengthen relationships through face-to-face and virtual engagement</li>
+  <li>Aligning your organisation with this important field of health and welfare provides you with the opportunity to consolidate relationships to a highly targeted audience involved in childhood disabilities.</li>
+  <li>Participate in the world’s premier Conference for childhood disabilities with over 1,500 healthcare professionals expected all under one roof</li>
+  <li>Connect with key decision makers in multidisciplinary scientific education for health professionals and researchers dedicated to the wellbeing of people with childhood-onset disabilities</li>
+  <li>Ensure your organisation stays abreast of current developments and research and use this information to stay ahead of the market</li>
+  <li>The virtual platform will enable you to:</li>
+  <ul>
+    <li>Develop business connections and strengthen relationships</li>
+    <li>Generate and track leads</li>
+    <li>Reach a global audience</li>
+    <li>Receive detailed data analytics</li>
+    <li>Share videos and brochures</li>
+    <li>Live chat</li>
+    <li>Ensure longevity – your information will remain on the platform for a period of time post event</li>
+  </ul>
 </ul>
 
-<a href="./downloads/IAACD2022-Prospectus.pdf" target="_blank" class="button d-block text-center my-4">Download the Supporter and Exhibition Prospectus</a>
-<a href="./downloads/IAACD2022-Application-Form.pdf" target="_blank" class="button d-block text-center my-4">Download the Supporter and Exhibitor Application form</a>
+<h4>Interested?  For further information please contact:</h4>
+<p><b>Andrea O’Sullivan</b> <br>
+  <em>Sponsorship & Exhibition Manager</em> <br>
+  <a href="mailto:sponsorship@bettertogether2022.org">sponsorship@bettertogether2022.org</a> <br>
+  Tel: +617 3255 1002</p>
 
+<!-- <a href="./downloads/IAACD2022-Prospectus.pdf" target="_blank" class="button d-block text-center my-4">Download the Supporter and Exhibition Prospectus</a>
+<a href="./downloads/IAACD2022-Application-Form.pdf" target="_blank" class="button d-block text-center my-4">Download the Supporter and Exhibitor Application form</a> -->
+<!--
 <h2 class="mt-5">Supported by</h2>
 
 <div class="row d-flex align-items-center">
@@ -31,7 +47,7 @@
   <div class="col-md-4">
     <a href="https://www.melbournecb.com.au/"><img src="./img/mcb.gif" class="m-5"></a>
   </div>
-</div>
+</div> -->
 
 
 <?php
