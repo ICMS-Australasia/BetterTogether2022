48b7d07d2ce54e6b70968ebd09e38836af3c9754
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-09-03 11:23:53 +1000

    Updated link

--- ambassadors.php
+++ ambassadors.php
@@ -15,7 +15,7 @@
  <ul>
    <li class="mb-2"><a href="downloads/A4-Promotional-Poster.pdf" target="_blank">A4 Promotional Poster</a></li>
    <li class="mb-2"><a href="downloads/Email-Signature.gif" target="_blank">Email Signature</a></li>
-   <li class="mb-2"><a href="downloads/PowerPoint-Template-and-Titles-Slide.pptx" target="_blank">PowerPoint Template and Title Slide</a></li>
+   <li class="mb-2"><a href="downloads/PowerPoint-Template-and-Titles-Slide.zip" target="_blank">PowerPoint Template and Title Slide</a></li>
    <li class="mb-2"><a href="downloads/Zoom-Backgrounds.zip" target="_blank">Zoom Backgrounds</a></li>
  </ul>
 
