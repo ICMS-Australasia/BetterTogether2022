fa465974fb04de2c985096b54c88b55c08211f06
jamesc0901 <jamesc@icmsaust.com.au>
2021-09-13 16:32:45 +1000

    updated

--- index.php
+++ index.php
@@ -1,96 +1,95 @@
-<?php
-    $title = "Better Together 2022";
-    include('include/header.php');
- ?>
-            <div class="col-12">
-                <div class="alert alert-secondary p-5 mt-4">
-                  <p class="mt-0">The current COVID-19 outbreaks throughout Australia have created increasing uncertainty about travel both within Australia and Internationally. Currently there is almost no travel across state borders in Australia, and the likelihood of international borders opening early in 2022 seems remote.</p>
-                  <p>Because of these uncertainties, the Better Together 2022 Organising Committee, in consultation with the AusACPDM Board and IAACD Steering Group, have made the decision to move to a fully virtual Conference in March 2022.</p>
-                  <p>This decision has been made now in advance before there are too many financial implications to the Host Academy, to those wishing to make plans to attend, and to allow time for the Committee to put together the best virtual conference possible for our participants.</p>
-                  <p>We thank everyone for their support and understanding in this decision, and we are all very much looking forward to a day when we can all safely come together again.</p>
-                  <p>We are Better Together, and we can be Better Together virtually as well as in person.</p>
-                  <p class="mb-0"><b>Warmest regards</b> <br>
-                    Better Together 2022 Organising Committee</p>
-                </div>
-            </div>
-            <div class="col-12 my-5 banner-welcome">
-                <a href="./welcome.php">
-                    <div>
-                        <img src="./img/bg2.jpg" class="img-fluid" width="100%">
-                        <p class="position-absolute my-0">Welcome to the <br><span>11<sup>th</sup> AusACPDM biennial</span><br> and <span>3rd IAACD triennial</span> <br>International Conference <br><br> <span><u>Learn more</u></span></p>
-                    </div>
-                </a>
-            </div>
-            <div class="col-12 col-md-6 mb-5 banner-eoi">
-                <a href="./registration.php">
-                    <img src="./img/banner-eoi.png" class="img-fluid" width="100%">
-                    <p class="position-absolute my-0">Register <br> <span>Now!</span></p>
-                </a>
-            </div>
-            <div class="col-12 col-md-6 mb-5 banner-covid">
-                <a href="https://www.youtube.com/watch?v=7qobe89Xss0&ab_channel=ICMSAustralasia" target="_blank">
-                    <img src="./img/banner-video.jpg" class="img-fluid" width="100%">
-                    <p class="position-absolute my-0">Introducing Better Together 2022</p>
-                </a>
-                <!-- <div class="embed-responsive embed-responsive-16by9 mt-4">
-                  <iframe width="100%" height="100%" src="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div> -->
-            </div>
-            <!-- <div class="col-6 mb-5 banner-member">
-                <a href="./">
-                    <img src="./img/banner-member.jpg" class="img-fluid " width="100%">
-                    <p class="position-absolute">Become a member</p>
-                </a>
-            </div> -->
-            <div class="col-12 col-lg-8">
-                <table class="key-dates" cellpadding="15" cellspacing="0" border="0" width="100%">
-                    <tr align="center" class="heading"><th>Key Dates</th></tr>
-                    <tr align="center" class="sub-heading"><th><a href="./focused-symposia.php">Focused Symposia - now announced</a></th></tr>
-                    <tr align="center" class="sub-heading"><th><a href="./pre-conference-sessions.php">Pre-Conference Sessions - now announced</a></th></tr>
-                    <tr align="center" class="sub-heading"><th>Call for Abstracts - now closed</th></tr>
-                        <tr align="center" bgcolor="#f2f2f2"><td>Announced August 2021</td></tr>
-                    <tr align="center" class="sub-heading"><th><a href="./scholarships.php">Scholarship Applications - now open</a></th></tr>
-                        <tr align="center"><td>Closes 5 September 2021</td></tr>
-                        <tr align="center" bgcolor="#f2f2f2"><td>Announced October 2021</td></tr>
-                    <tr align="center" class="sub-heading"><th><a href="./registration.php">Registration - now open</a></th></tr>
-                        <tr align="center"><td>Early Bird closes 1 November 2021</td></tr>
-                        <tr align="center" class="sub-heading"><th>Late Breaking Abstracts</th></tr>
-                        <tr align="center"><td>Opens September 2021</td></tr>
-                        <tr align="center" bgcolor="#f2f2f2"><td>Closes December 2021</td></tr>
-                        <tr align="center"><td>Announced January 2022</td></tr>
-                    <tr align="center" class="conference-heading"><th>Better Together 2022</th></tr>
-                        <tr align="center"  class="conference"><th>Pre-conference Sessions, 1 - 2 March 2022</th></tr>
-                        <tr align="center" class="conference"><th>Conference dates 2 - 5 March 2022</th></tr>
-                </table>
-            </div>
-            <div class="col-12 col-lg-4 mt-5 mt-lg-0">
-                <div class="card">
-                    <a href="./committee.php">
-                        <img src="./img/imms.jpg" class="card-img-top" alt="Committee">
-                        <div class="card-body">
-                            <h5 class="card-title">Committee</h5>
-                            <p class="card-text">Meet the team that make it all happen. <u>View more</u></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="card mt-4">
-                    <a href="./destination-discovery.php">
-                        <img src="./img/destination-discovery.png" class="card-img-top" alt="Committee">
-                        <div class="card-body">
-                            <h5 class="card-title">Destination Discovery</h5>
-                            <p class="card-text">Science meets lifestyle, your free time well spent. <u>View more</u></p>
-                        </div>
-                    </a>
-                </div>
-                <div class="card mt-4">
-                    <a href="./support-abstract.php">
-                        <img src="./img/presentation-tips.png" class="card-img-top" alt="Committee">
-                        <div class="card-body">
-                            <h5 class="card-title">Presentation Tips</h5>
-                            <p class="card-text">Learn some helpful tips and tricks for your presentation. <u>View more</u></p>
-                        </div>
-                    </a>
-                </div>
-<?php
-    include('include/footer.php');
- ?>
+<?php
+    $title = "Better Together 2022";
+    include('include/header.php');
+ ?>
+            <div class="col-12">
+                <div class="alert alert-secondary p-5 mt-4">
+                  <p class="mt-0">The current COVID-19 outbreaks throughout Australia have created increasing uncertainty about travel both within Australia and Internationally. Currently there is almost no travel across state borders in Australia, and the likelihood of international borders opening early in 2022 seems remote.</p>
+                  <p>Because of these uncertainties, the Better Together 2022 Organising Committee, in consultation with the AusACPDM Board and IAACD Steering Group, have made the decision to move to a fully virtual Conference in March 2022.</p>
+                  <p>This decision has been made now in advance before there are too many financial implications to the Host Academy, to those wishing to make plans to attend, and to allow time for the Committee to put together the best virtual conference possible for our participants.</p>
+                  <p>We thank everyone for their support and understanding in this decision, and we are all very much looking forward to a day when we can all safely come together again.</p>
+                  <p>We are Better Together, and we can be Better Together virtually as well as in person.</p>
+                  <p class="mb-0"><b>Warmest regards</b> <br>
+                    Better Together 2022 Organising Committee</p>
+                </div>
+            </div>
+            <div class="col-12 my-5 banner-welcome">
+                <a href="./welcome.php">
+                    <div>
+                        <img src="./img/bg2.jpg" class="img-fluid" width="100%">
+                        <p class="position-absolute my-0">Welcome to the <br><span>11<sup>th</sup> AusACPDM biennial</span><br> and <span>3rd IAACD triennial</span> <br>International Conference <br><br> <span><u>Learn more</u></span></p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 mb-5 banner-eoi">
+                <a href="./registration.php">
+                    <img src="./img/banner-eoi.png" class="img-fluid" width="100%">
+                    <p class="position-absolute my-0">Register <br> <span>Now!</span></p>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 mb-5 banner-covid">
+                <a href="https://www.youtube.com/watch?v=7qobe89Xss0&ab_channel=ICMSAustralasia" target="_blank">
+                    <img src="./img/banner-video.jpg" class="img-fluid" width="100%">
+                    <p class="position-absolute my-0">Introducing Better Together 2022</p>
+                </a>
+                <!-- <div class="embed-responsive embed-responsive-16by9 mt-4">
+                  <iframe width="100%" height="100%" src="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div> -->
+            </div>
+            <!-- <div class="col-6 mb-5 banner-member">
+                <a href="./">
+                    <img src="./img/banner-member.jpg" class="img-fluid " width="100%">
+                    <p class="position-absolute">Become a member</p>
+                </a>
+            </div> -->
+            <div class="col-12 col-lg-8">
+                <table class="key-dates" cellpadding="15" cellspacing="0" border="0" width="100%">
+                    <tr align="center" class="heading"><th>Key Dates</th></tr>
+                    <tr align="center" class="sub-heading"><th><a href="./focused-symposia.php">Focused Symposia - now announced</a></th></tr>
+                    <tr align="center" class="sub-heading"><th>Pre-Conference Workshop - now announced</th></tr>
+                    <tr align="center" class="sub-heading"><th>Call for Abstracts - now closed</th></tr>
+                        <tr align="center" bgcolor="#f2f2f2"><td>Announced September 2021</td></tr>
+                    <tr align="center" class="sub-heading"><th><a href="./scholarships.php">Scholarship Applications - now closed</a></th></tr>
+                        <tr align="center"><td>Announced October 2021</td></tr>
+                    <tr align="center" class="sub-heading"><th><a href="./registration.php">Registration - now open</a></th></tr>
+                        <tr align="center"><td>Early Bird closes 1 November 2021</td></tr>
+                        <tr align="center" class="sub-heading"><th>Late Breaking Abstracts</th></tr>
+                        <tr align="center"><td>Opens 30 September 2021</td></tr>
+                        <tr align="center" bgcolor="#f2f2f2"><td>Closes 1 December 2021</td></tr>
+                        <tr align="center"><td>Announced December 2021</td></tr>
+                    <tr align="center" class="conference-heading"><th>Better Together 2022</th></tr>
+                        <tr align="center" class="conference"><th>Pre-conference Workshop 1 March 2022</th></tr>
+                        <tr align="center" class="conference"><th>Conference dates 2 - 5 March 2022</th></tr>
+                </table>
+            </div>
+            <div class="col-12 col-lg-4 mt-5 mt-lg-0">
+                <div class="card">
+                    <a href="./committee.php">
+                        <img src="./img/imms.jpg" class="card-img-top" alt="Committee">
+                        <div class="card-body">
+                            <h5 class="card-title">Committee</h5>
+                            <p class="card-text">Meet the team that make it all happen. <u>View more</u></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="card mt-4">
+                    <a href="./destination-discovery.php">
+                        <img src="./img/destination-discovery.png" class="card-img-top" alt="Committee">
+                        <div class="card-body">
+                            <h5 class="card-title">Destination Discovery</h5>
+                            <p class="card-text">Science meets lifestyle, your free time well spent. <u>View more</u></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="card mt-4">
+                    <a href="./support-abstract.php">
+                        <img src="./img/presentation-tips.png" class="card-img-top" alt="Committee">
+                        <div class="card-body">
+                            <h5 class="card-title">Presentation Tips</h5>
+                            <p class="card-text">Learn some helpful tips and tricks for your presentation. <u>View more</u></p>
+                        </div>
+                    </a>
+                </div>
+<?php
+    include('include/footer.php');
+ ?>
--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -1,57 +1,59 @@
-<?php
-    $title = "Terms & Conditions";
-    include('include/header.php');
- ?>
-
- <h2>Cancellation Policy</h2>
- <h3>Deadline: 1 January 2022</h3>
- <p>Cancellations must be notified in writing (e-mail is sufficient) to the Conference Secretariat. Cancellations made by 1 January 2022 will result in a full refund less $150 to cover administration costs. As an alternative to cancellation, your registration may be transferred to another person by 1 January 2022 without incurring any cost penalty. There will be no cancellation allowance for failure to secure a visa to Australia. There will be no refunds for Virtual registrations. In the event of the Conference becoming completely virtual the delegate will receive a refund of the difference between the in-person and virtual cost. Delegates can pay the difference to upgrade from a virtual registration to the equivalent in-person registration at any time without incurring any cost penalty. Delegates must note that some social functions may reach their capacity limits and tickets are only guaranteed on a first come first served basis. The Conference Secretariat must be advised in writing of any alterations or transfers. The Organising Committee reserves final decision on a case by case basis for granting a cancellation in extenuating circumstances. It is recommended that travel insurance be secured personally upon registration.</p>
-
- <h3>Conference Postponement</h3>
- <p>In the event of a postponement all registrations will automatically be transferred to the new Conference dates. All components of your registration including accommodation bookings, Welcome Reception RSVP and other social function tickets will also be transferred. If you are unable to attend on the new dates, you may contact the Conference Secretariat to request a transfer of your registration to a colleague at no cost, or request a cancellation and refund which will incur a $150 administration fee up until the Registration Cancellation Deadline.</p>
-
- <h3>Conference Cancellation</h3>
- <p>In the event of the cancellation of the Conference, all registrations and associated components of attendance will be cancelled and amounts refunded in full.</p>
-
- <h3>Australian State Borders Closed</h3>
- <p>In the event that the state borders are re-closed between Monday 28 February – Sunday 6 March 2022, from your departing city to Melbourne and you are therefore unable to attend the Conference in-person, your registration will be transferred to a virtual registration and the difference will be refunded, inclusive of any social function tickets. For any accommodation bookings the Conference Secretariat will contact the hotel and put in a request for cancellation, however the cancellation fee will be at the discretion of each individual hotel.</p>
-
- <h3>International Borders Closed</h3>
- <p>We ask all international delegates to carefully monitor the Australian travel restrictions and exemptions: <a href="https://covid19.homeaffairs.gov.au/travel-restrictions" target="_blank">https://covid19.homeaffairs.gov.au/travel-restrictions</a> <br>
-   In the event that international borders open for international travel and delegates register during this time, and then borders are re-closed between Monday 28 February - Sunday 6 March 2022, from your departing Country and Melbourne and you are no longer able to attend the Conference in-person, your registration will be transferred to a virtual registration and the difference will be refunded, inclusive of any social function tickets.</p>
-
- <h3>Disclaimer</h3>
- <p>In registering for this Conference relevant details (name/country) will be incorporated into a delegate list for the benefit of all delegates, and also may be made available (if necessary) to parties directly related to the Conference including venues, accommodation providers, sponsors, key suppliers and future Conferences. Your name and contact information may be used by the Conference Secretariat and other approved stakeholder, for marketing and managing this and future events of this type.</p>
-
- <h2>Accommodation Cancellations</h2>
- <h3>Deadline: 24 January 2022</h3>
- <p>No accommodation payments will be refunded on or after Friday 24 January 2022. <br>
-   Participants who confirm an accommodation booking with a credit card or full pre-payment and who wish to cancel on or after Friday 24 January 2022 are liable for the full cost of their accommodation nights cancelled. To cancel your reservation, please contact The Accommodation Secretariat at <a href="mailto:accommodation@bettertogether2022.org">accommodation@bettertogether2022.org</a>. Name changes to a reservation can be made at no charge.</p>
- <p>Delegates who confirm attendance by paying a minimum of the first night and do not advise of cancellation before 24 January 2022 the ‘no show’ rate, as penalised by the hotel, will be charged to you and your payment will not be refunded. This charge is calculated as the room rate per night multiplied by the dates booked and secured. Please ensure that accommodation dates are correct at time of booking and before 24 January 2022 if any changes are required.</p>
-
- <h3>Change of accommodation booking</h3>
- <p>Any change in booking must be made in writing to the Conference Secretariat and NOT directly to the hotel before 24 January 2022. Any changes received after 24 January 2022 will be at the hotels discretion and based on availability.</p>
-
- <h3>Early arrival</h3>
- <p>As check in at most hotels is not until 1400, it is possible that your room may not be ready for immediate occupation on your early arrival. Whilst hotels will do everything possible to accommodate early arrivals, if you wish to be guaranteed immediate occupation you can pay an extra day’s tariff for the night before you are due to arrive. If you wish to take this precaution, please advise the Conference Secretariat so we can notify the hotel that your room is required early.</p>
-
- <h3>Late arrival</h3>
- <p>Please indicate if you will arrive at your hotel after 1800. Failure to do so might result in your room being released.</p>
-
-
-<h2>Enquiries</h2>
-<p>Please direct all enquiries to the Conference Secretariat.</p>
-
-<p><b>Registration Enquiries</b> <br>
-  P: +61 3 9682 0500 <br>
-  <a href="mailto:Registration@bettertogether2022.org">Registration@bettertogether2022.org</a></p>
-
-<p><b>Accommodation Enquires</b> <br>
-  P: +61 2 9254 5000 <br>
-  <a href="mailto:Accommodation@bettertogether2022.org">Accommodation@bettertogether2022.org</a></p>
-
-
-
-<?php
-    include('include/footer.php');
- ?>
+<?php
+    $title = "Terms & Conditions";
+    include('include/header.php');
+ ?>
+
+ <!-- <h2>Cancellation Policy</h2>
+ <h3>Deadline: 1 January 2022</h3>
+ <p>Cancellations must be notified in writing (e-mail is sufficient) to the Conference Secretariat. Cancellations made by 1 January 2022 will result in a full refund less $150 to cover administration costs. As an alternative to cancellation, your registration may be transferred to another person by 1 January 2022 without incurring any cost penalty. There will be no cancellation allowance for failure to secure a visa to Australia. There will be no refunds for Virtual registrations. In the event of the Conference becoming completely virtual the delegate will receive a refund of the difference between the in-person and virtual cost. Delegates can pay the difference to upgrade from a virtual registration to the equivalent in-person registration at any time without incurring any cost penalty. Delegates must note that some social functions may reach their capacity limits and tickets are only guaranteed on a first come first served basis. The Conference Secretariat must be advised in writing of any alterations or transfers. The Organising Committee reserves final decision on a case by case basis for granting a cancellation in extenuating circumstances. It is recommended that travel insurance be secured personally upon registration.</p>
+-->
+ <p> Once your registration has been completed, there will be no refunds available. The Conference content will remain online and active for 3 months post Conference and remain accessible for registered delegates to view at any time during this period. Your registration may be transferred to another person by 1 January 2022. The Conference Secretariat must be advised in writing of any alternations or transfers by this date.</p>
+ 
+ <h3>Conference Postponement</h3>
+ <p>In the event of a postponement all registrations will automatically be transferred to the new Conference dates. All components of your registration including Workshop tickets will also be transferred.</p>
+
+ <h3>Conference Cancellation</h3>
+ <p>In the event of the cancellation of the Conference, all registrations and associated components of attendance will be cancelled and amounts refunded in full.</p>
+
+<!-- <h3>Australian State Borders Closed</h3>
+ <p>In the event that the state borders are re-closed between Monday 28 February – Sunday 6 March 2022, from your departing city to Melbourne and you are therefore unable to attend the Conference in-person, your registration will be transferred to a virtual registration and the difference will be refunded, inclusive of any social function tickets. For any accommodation bookings the Conference Secretariat will contact the hotel and put in a request for cancellation, however the cancellation fee will be at the discretion of each individual hotel.</p>
+
+ <h3>International Borders Closed</h3>
+ <p>We ask all international delegates to carefully monitor the Australian travel restrictions and exemptions: <a href="https://covid19.homeaffairs.gov.au/travel-restrictions" target="_blank">https://covid19.homeaffairs.gov.au/travel-restrictions</a> <br>
+   In the event that international borders open for international travel and delegates register during this time, and then borders are re-closed between Monday 28 February - Sunday 6 March 2022, from your departing Country and Melbourne and you are no longer able to attend the Conference in-person, your registration will be transferred to a virtual registration and the difference will be refunded, inclusive of any social function tickets.</p>
+-->
+ <h3>Disclaimer</h3>
+ <p>In registering for this Conference relevant details (name/country) will be incorporated into a delegate list for the benefit of all delegates, and also may be made available (if necessary) to parties directly related to the Conference including sponsors, key suppliers and future Conferences. Your name and contact information may be used by the Conference Secretariat and other approved stakeholder, for marketing and managing this and future events of this type.</p>
+
+ <!-- <h2>Accommodation Cancellations</h2>
+ <h3>Deadline: 24 January 2022</h3>
+ <p>No accommodation payments will be refunded on or after Friday 24 January 2022. <br>
+   Participants who confirm an accommodation booking with a credit card or full pre-payment and who wish to cancel on or after Friday 24 January 2022 are liable for the full cost of their accommodation nights cancelled. To cancel your reservation, please contact The Accommodation Secretariat at <a href="mailto:accommodation@bettertogether2022.org">accommodation@bettertogether2022.org</a>. Name changes to a reservation can be made at no charge.</p>
+ <p>Delegates who confirm attendance by paying a minimum of the first night and do not advise of cancellation before 24 January 2022 the ‘no show’ rate, as penalised by the hotel, will be charged to you and your payment will not be refunded. This charge is calculated as the room rate per night multiplied by the dates booked and secured. Please ensure that accommodation dates are correct at time of booking and before 24 January 2022 if any changes are required.</p>
+
+ <h3>Change of accommodation booking</h3>
+ <p>Any change in booking must be made in writing to the Conference Secretariat and NOT directly to the hotel before 24 January 2022. Any changes received after 24 January 2022 will be at the hotels discretion and based on availability.</p>
+
+ <h3>Early arrival</h3>
+ <p>As check in at most hotels is not until 1400, it is possible that your room may not be ready for immediate occupation on your early arrival. Whilst hotels will do everything possible to accommodate early arrivals, if you wish to be guaranteed immediate occupation you can pay an extra day’s tariff for the night before you are due to arrive. If you wish to take this precaution, please advise the Conference Secretariat so we can notify the hotel that your room is required early.</p>
+
+ <h3>Late arrival</h3>
+ <p>Please indicate if you will arrive at your hotel after 1800. Failure to do so might result in your room being released.</p> -->
+
+
+<h2>Enquiries</h2>
+<p>Please direct all enquiries to the Conference Secretariat.</p>
+
+<p><b>Registration Enquiries</b> <br>
+  P: +61 3 9682 0500 <br>
+  <a href="mailto:Registration@bettertogether2022.org">Registration@bettertogether2022.org</a></p>
+
+<!-- <p><b>Accommodation Enquires</b> <br>
+  P: +61 2 9254 5000 <br>
+  <a href="mailto:Accommodation@bettertogether2022.org">Accommodation@bettertogether2022.org</a></p> -->
+
+
+
+<?php
+    include('include/footer.php');
+ ?>
