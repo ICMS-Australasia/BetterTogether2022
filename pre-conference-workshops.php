d05fdbf699c33276825d344fc32d0a6ade6c461f
jamesc0901 <jamesc@icmsaust.com.au>
2021-09-20 11:02:15 +1000

    updated

---
+++ pre-conference-workshops.php
@@ -0,0 +1,155 @@
+<?php
+    $title = "Pre-Conference Workshops";
+    include('include/header.php');
+ ?>
+
+<p>
+Pre-conference Workshops will be taking place on Tuesday 1st March 2022.
+</br></br>
+These are split into either Short or Long categories. Short workshops will be approximately 2-3 hours, Long workshops will be 4 hours or more. 
+The exact timetable and format of each individual Workshop will be confirmed by the Workshop Facilitators.
+</br></br>
+You are able to purchase an add-on to your registration to attend any these Workshops through the online registration form.
+</br></br>
+*Most of these Workshops are being held concurrently, therefore we recommend you only select one to avoid any clashes
+</p>
+
+<h3>SHORT WORKSHOPS</h3>
+<p>
+<b>International Genomics</b></br>
+Michael Fahey, Michael Kruer, Joseph Getz, Sarah McIntyre, Yana Wilson
+</br></br>
+<b>International CP Registries</b></br>
+Sarah McIntyre, Shona Goldsmith, Hayley Smithers Sheedy, Guro Anderson
+</br></br>
+<b>Physical activity participation for people with disability across the lifespan: shifting to sustaining participation</b></br>
+Sian Williams, Jan Willem Gorter
+</br></br>
+<b>Selective Dorsal Rhizotomy experiences around the world</b></br>
+Nelleke Langerak, Tom Novachek, Nico Enslin, Kristian Aquilina, Simon Paget, Marcie Ward
+</p>
+
+<h3>LONG WORKSHOPS</h3>
+<p>
+<b>Improving the Health and Well Being of Adults with Childhood onset Disability</b></br>
+Mark Peterson, Dinah Reddihough, Christine Imms
+</br></br>
+<b>Muscle in CP</b></br>
+Richard Lieber, Sudarshan Dayanidhi, Tandy Hastings-ison
+</br></br>
+<b>What’s new in early diagnosis and early intervention</b></br>
+Iona Novak, Cath Morgan, Anki Eliasson, Stacy Dusing, Ros Boyd, Alicia Spittle
+</br></br>
+<b>Preclinical and clinical studies: reducing perinatal injury</b></br>
+MinYoung Kim, Suzie Miller, Iona Novak
+</p>
+<!-- <p>Pre-conference sessions are being held on Tuesday 1st and Wednesday 2nd March 2022. The format of these sessions will be either hybrid or virtual and available for purchase through the online registration form.</p>
+
+ <table class="program mt-5" width="100%" cellpadding="10">
+     <tr class="heading" align="center">
+       <th  colspan="7">Tuesday 1 March 2022</th>
+     </tr>
+</table>
+
+
+ <p><b>Improving the health and wellbeing of adults with cerebral palsy </b></p>
+ <p><em>Mark Peterson, Edward Hurvitz, Jan Willem Gorter, Elisabet Rodby-Bousquet, Sarah Reedman, Dinah Reddihough, Sevastine Katsakis, David Coghill, Prue Morgan, Rob Carter</em></p>
+ <p>This workshop will include international experts discussing how we can meet the medical, rehabilitation and participatory needs of adults with cerebral palsy. This will include the role of physical and mental health and prevalence and risk factors for cardiovascular and musculoskeletal disease. We will integrate knowledge translation: embedding the priorities of adults living with cerebral palsy with a focus on building supportive health and disability service environments and
+incorporating health economics into research to drive policy and practice.</p>
+
+<hr>
+
+ <p><b>Muscle in cerebral palsy</b></p>
+ <p><em>Richard Leiber, Sudarshan Dayanidhi, Tandy Hastings-Ison</em></p>
+ <p>This workshop across seven different sessions will focus on structural, functional, and adaptive changes in muscle for people who live with cerebral palsy. Multi-disciplinary speakers will address a range of topics affecting muscle including Botulinum toxin, from basic science research to clinical management, with audience participation regarding optimal muscle health</p>
+
+<hr>
+
+ <p><b>Early Intervention for infants with cerebral palsy or born preterm</b></p>
+ <p><em>Cathy Morgan, Iona Novak, Ann Christian Eliasson, Stacey Dusing, Ros Boyd, Alicia Spittle, Laura Prosser, Regina Harbourne </em></p>
+ <p>This workshop will outline the current state of the science in 2022 on early intervention for cerebral palsy. This pre-conference symposium will include a multidisciplinary global panel of experts and present the key ingredients and results of novel early intervention clinical trials. The day will include discussion on implementation strategies, statistical challenges and future research opportunities.</p>
+
+<hr>
+
+
+ <p>There are two half day pre-conference sessions, delegates can select to attend both the AM and PM session to create a full day program if they wish.</p>
+
+
+ <p><b>International CP Genomics Consortium</b></p>
+ <p><em>Michael Fahey, Michael Kruer, Joseph Getz, Sarah McIntyre, Yana Wilson</em></p>
+ <p>This pre-conference workshop will discuss the latest evidence, current controversies and potential solutions related to genetics and cerebral palsy. The workshop includes international experts in the field from a variety of clinical specialties to provide a global view on current clinical and research experiences.</p>
+
+<hr>
+
+ <p><b>International CP Registers</b></p>
+ <p><em>Hayley Smithers-Sheedy, Sarah McIntyre, Kate Himmelmann, Guro L Andersen, Gulam Khandaker and Michael Shevell.</em></p>
+ <p>The 5th World CP Register and Surveillance Congress will be held in conjunction with the IAACD conference. This is a great opportunity to meet those who are defining the Epidemiology of CP and learn together! This interactive workshop is suitable for anyone interested in CP registers and surveillance including establishment, methods, operations, utility, training and recent findings.</p>
+
+<table class="program mt-5" width="100%" cellpadding="10">
+    <tr class="heading" align="center">
+      <th  colspan="7">Wednesday 2 March 2022</th>
+    </tr>
+</table>
+
+
+ <p><b>Let’s get strategic about physical activity participation for people with disability across the lifespan</b></p>
+ <p><em>Sian Williams, Gaela Kilgour, Olaf Verschuren, Sarah Reedman, Nora Shields, and Jan Willem Gorter</em></p>
+ <p>This symposium will focus on a lifespan and all abilities approach to promote 24 hour activity guidelines including sleep in people with disability. Panellists will discuss health and wellbeing benefits of physical activity, behaviour change as relevant for clinicians, and participation as a key to achieve sustained lifelong benefits.</p>
+
+<hr>
+
+ <p><b>Preclinical studies, neuroprotection, prevention and neural repair</b></p>
+ <p><em>MinYoung Kim, Suzie Miller, Iona Novak, Kylie Crompton, So Yoon Ah, Atul Malhorta, Madison Paton, Megan Finch Edmonson,</em></p>
+ <p>Cell-based therapies have neuroprotective and neuroregenerative potential for perinatal brain injury. This symposium brings together international experts to provide a status update on clinical trials for cell therapies, and how neuroprotective efficacy might be enhanced. We will also examine what is on the horizon for novel therapies particularly aimed at the preterm brain</p>
+
+<hr>
+
+ <p><b>Early Detection of cerebral palsy - experiences and challenges</b></p>
+ <p><em>Cath Morgan, Iona Novak, Atul Malhotra, Esther Tantsis, Dr Samanmali Sumanasena, Sian Williams</em></p>
+ <p>International Guidelines on early detection of cerebral palsy have been available for the past 5 years and have seen changes in practice globally. In this workshop, global experiences of translation will be discussed. Examples of clinical conundrums and implementation challenges faced by clinicians in the early diagnosis of CP will be presented.</p>
+
+<hr>
+
+ <p><b>Selective Dorsal Rhizotomy experiences around the world</b></p>
+ <p><em>Nelleke Langerak, Tom Novachek, Nico Enslin, Kristian Aquilina, Simon Paget, Marcie Ward</em></p>
+ <p>This will be a global, multidisciplinary course reviewing selective dorsal rhizotomy (SDR) including: spasticity management overview, SDR techniques, practices and experiences, post-operative management, policies and services, short-and long-term outcomes, and future of SDR. We aim to help clinicians and caregivers of children with spastic CP with their management plan.</p>
+
+<hr>
+
+
+ <p>The below pre-conference sessions are 90-minutes long, mix and match one AM and one PM to make your own custom half-day pre-conference session.</p>
+
+ <p><b><u>AM Symposia</u></b></p>
+ <p>The following AM Symposia will run concurrently from 8:30am – 10:00am (AEDT), please select to attend only one from the online registration form</p>
+
+ <p><b>Exploring inequities in service provision for Indigenous and First Nations women, their babies and children with cerebral palsy</b></p>
+ <p><em>Alexandra Sorhage, Danny DeLore, Tan Martin, Juanita Sherwood, Michael Shevell, Anna Mackey, Sian Williams, Hayley Smithers-Sheedy, Sarah McIntyre, Sue Woolfenden</em></p>
+ <p>This symposium will identify success stories and areas of continuing inequity for First Nations’ people with cerebral palsy (CP). Participants will be encouraged to discuss and reflect on reasons for and approaches to addressing potential inequity within their own work, and will leave with ideas to apply within their own context.</p>
+
+<hr>
+
+ <p><b>Implementation, challenges and impact of four parent-delivered training programmes for children with developmental disabilities in lower and middle income countries (LMIC)</b></p>
+ <p><em>Hans Forssberg, Gillian Saloojee, Rachel Lassman, Katherine Benfer, Pamela Dixon</em></p>
+ <p>This symposium focuses on the implementation of four parent-led training programmes for children with developmental disabilities living in resource-constrained settings. Presenters will describe the training, implementation and outcomes of each programme. This will be followed by a discussion how challenges common to all programmes were addressed.</p>
+
+<hr>
+
+ <p><b><u>PM Symposia</u></b></p>
+ <p>The following PM Symposia will run concurrently from 10:30am – 12:00pm (AEDT), please select to attend only one from the online registration form</p>
+
+ <p><b>Cultural influences on early screening and intervention for Aboriginal and Torres Strait Islander infants at risk of Neurodevelopmental Disabilities</b></p>
+ <p><em>Kath Benfer, Marjad Page, Dianne Shanley, Leann Ramsamy, Carly Luke</em></p>
+ <p>Little is known about the Aboriginal and Torres Strait Islander construct of disability. This understanding has significant implications for how First Nations communities engage with child development and disability services, including screening and ‘intervention’ programs. With greater awareness of the importance of earlier screening and intervention based on the International Clinical Practice Guideline (Novak 2017), combined with increasing availability of disability services under NDIS, it is pertinent to focus on the role of culture in such programs.</p>
+
+<hr>
+
+ <p><b>Better Research Together: Conversations with Researchers and Young People</b></p>
+ <p><em>Linda Nguyen, Bente van Oort, Eline van der Meulen, Hanae Davis, Samantha Bellefeuille, Jordan Matthews, Christopher Morris, Jan Willem Gorter, Anita Franklin, and Marjolijn Ketelaar</em></p>
+ <p>This symposium, co-presented by young people and researchers, aims to: Share the experiences, opportunities, and challenges of research partnerships from Canada, the Netherlands, and the United Kingdom; and cultivate awareness of different models to guide engagement with young people in health research. </p>
+
+<hr>
+-->
+
+<?php
+    include('include/footer.php');
+ ?>
